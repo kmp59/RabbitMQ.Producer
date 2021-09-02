ae28da380900ce6247e7938e167835281ee31267
kmp59 <kmp59@njit.edu>
2021-09-02 01:02:28 -0400

    Added Initial Code

---
+++ RabbitMQ.Producer/README.md
@@ -0,0 +1,13 @@
+<div align="center">
+
+# QuorumQueue_Producer
+
+</div>
+
+## - RabbitMQ Producer App to publish message to Quorum Queue
+
+1. Creates Connection to the local RabbitMQ server
+
+2. Creates model to transfer data to the server
+
+3. Send data to the Quorum Queue using Basic Publish method
---
+++ RabbitMQ.Producer/RabbitMQ.Producer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace RabbitMQ.Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                //URI Formula: amqp://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORTNUMBER}
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+
+            using var conn = factory.CreateConnection();
+
+            using (var channel = conn.CreateModel())
+            {
+                var arguments = new Dictionary<string, object> { };
+                arguments.Add("x-queue-type", "quorum");
+
+                channel.QueueDeclare("demo-queue", durable: true, exclusive: false, autoDelete: false, arguments: arguments);
+                for(int i = 0; i < 10; i++)
+                {
+                    var message = new { Name = "Producer", Message = $"Hello+{i}" };
+                    var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+                    channel.BasicPublish(exchange: "", routingKey: "demo-queue", basicProperties: null, body: body);
+                }
+            }
+        }
+    }
+}
